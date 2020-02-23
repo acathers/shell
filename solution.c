f49c4ad76f8daf8acc678b1970cf3d3773e0116e
acathers <acathers@hawaii.edu>
2020-02-22 20:14:09 -1000

    working

--- solution.c
+++ solution.c
@@ -1,69 +1,114 @@
+#include <fcntl.h>
+#include <malloc.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <sys/stat.h>
 
-/* 
+char **parse_command(char *input);
+
+/*
  * A function that executes a command using fork and execv
  */
 void execute_plain(char *cmd, char *const argv[]) {
   int status;
   pid_t pid = fork();
-  if(pid == 0){ // child
-    execv(cmd,argv);
-    exit(-127);
-  } else if(pid > 0) {  // parent 
+  if (pid == 0) {  // child
+    execv(cmd, argv);
+    fprintf(stdout, "\t** Command failed **\n");
+  } else if (pid > 0) {  // parent
     waitpid(pid, &status, 0);
-    if(WEXITSTATUS(status)){
-    fprintf(stdout, "** Command successful **\n");
-    } else {
-    fprintf(stdout, "** Command failed **\n"); //only executes if execv returns(fails)
+    if (WIFEXITED(status)) {
+      fprintf(stdout, "** Command successful **\n");
     }
-  } else { // handle failed to fork
-    fprintf(stdout, "** Command failed **\n"); //used same message for failed to fork since assignment didn't have any specifics
+  } else {
+    fprintf(stdout, "** Command failed **\n");
   }
 }
 
-/* 
+/*
  * A function that executes a command using fork and execv, but
  * that redirects the command's output to a file
  */
+
 void execute_output_to_file(char *cmd, char *const argv[], char *filename) {
-
-
   int status;
   pid_t pid = fork();
-  if(pid == 0){ // child
+  if (pid == 0) {  // child
+    int original_stdout = dup(STDOUT_FILENO);
     int fd = open(filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-    dup2(fd, 1);
+    dup2(fd, STDOUT_FILENO);
     close(fd);
-    execv(cmd,argv);
-    fprintf(stdout, "** Command failed **\n"); //only executes if execv returns(fails)
-  } else if(pid > 0) {  // parent 
+    execv(cmd, argv);
+    dup2(original_stdout, STDOUT_FILENO);
+    fprintf(stdout, "\t** Command failed **\n");
+  } else if (pid > 0) {  // parent
     waitpid(pid, &status, 0);
-    if(WIFEXITED(status)){
-    fprintf(stdout, "** Command successful **\n");
-    } else {
-      fprintf(stdout, "** Command failed **\n"); //only executes if execv returns(fails)
+    if (WIFEXITED(status)) {
+      fprintf(stdout, "** Command successful **\n");
     }
-  } else { // handle failed to fork
-    fprintf(stdout, "** Command failed **\n"); //used same message for failed to fork since assignment didn't have any specifics
+  } else {
+    fprintf(stdout, "** Command failed **\n");
   }
-
-
 }
 
-/* 
+/*
  * A function that executes a command using fork and execv, but
  * that redirects the command's output to another command
  */
-void execute_output_to_other(char *cmd1, char *const argv1[], char *cmd2_with_argv2) {
-  fprintf(stdout,"Error: %s() is not implemented!\n\n", __FUNCTION__);
+void execute_output_to_other(char *cmd1, char *const argv1[],
+                             char *cmd2_with_argv2) {
+  int fds[2];
+  int status, status2;
+  pipe(fds);
 
-  // WRITE CODE HERE
+  pid_t pid1 = fork();
+  if (!pid1) {  
+    close(fds[1]);
+    close(0);
+    dup(fds[0]);
 
+    char **command = parse_command(cmd2_with_argv2);
+
+    execv(command[0], command);
+    perror("execv");
+  }
+
+  pid_t pid2 = fork();
+  if (!pid2) {  
+    close(fds[0]);
+    close(1);
+    dup(fds[1]);
+    execv(cmd1, argv1);
+    perror("execv");
+  }
+
+  waitpid(pid2, &status, 0);
+  close(fds[1]);
+  waitpid(pid1, &status2, 0);
+      if (WIFEXITED(status) && WIFEXITED(status2)) {
+      fprintf(stdout, "** Command successful **\n");
+    }else {
+      fprintf(stdout, "** Command failed **\n");
+    }
 }
 
+char **parse_command(char *input) {
+  char **command = malloc(8 * sizeof(char *));
+  char *separator = " ";
+  char *parsed;
+  int index = 0;
 
+  parsed = strtok(input, separator);
+  while (parsed != NULL) {
+    command[index] = parsed;
+    index++;
+
+    parsed = strtok(NULL, separator);
+  }
+
+  command[index] = NULL;
+  return command;
+}
